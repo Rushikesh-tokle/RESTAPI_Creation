e5e3594e35f2a7bc879dc2c4135340cf61a5cf4d
rushi <rushikesh.tokle@gmail.com>
2025-08-04 18:34:37 +0530

    RESTful API's creted Successfully Final Commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app=express();
 const port=8080;
 const path=require("path");
 const { v4: uuidv4}=require("uuid");
+const methodOverride=require("method-override");
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(express.json());
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
+app.use(methodOverride("_method"));
 
 
 let posts=[{
@@ -57,7 +59,20 @@ app.patch("/posts/:id",(req,res)=>{
   let newContent=req.body.content;
   let post=posts.find((p)=>id===p.id);
   post.content=newContent;
-    res.send("Patch Request Working");
+ console.log(newContent);
+    res.redirect("/posts");
+})
+
+app.get("/posts/:id/edit",(req,res)=>{
+     let {id} =req.params;
+     let post=posts.find((p)=>id===p.id);
+     res.render("edit.ejs",{post});
+})
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+     posts=posts.filter((p)=>id!==p.id);
+    res.redirect("/posts");
 })
 
 app.listen(port,()=>{
